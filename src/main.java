3267dae960e1ea651ae31acb539911bec1b879ce
ffa110 <ffabunayyan@hotmail.com>
2020-02-14 21:45:43 -0500

    Capture host IP Number functionality

--- src/main.java
+++ src/main.java
@@ -1,7 +1,37 @@
+import java.net.NetworkInterface;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class main {
+
+
 
     public static void main(String[] args)
     {
-        System.out.println("Thisi is a test!");
+        System.out.println("Host IP Number: " + getIP());
+
     }
+
+    public static String getIP() {
+
+        // String to store ip number
+        String getIP = "";
+
+        // capture ip number
+        InetAddress ip;
+        try {
+            // get local host
+            ip = InetAddress.getLocalHost();
+            // store ip number in "getIP" String
+            getIP = ip.getHostAddress().toString();
+        } catch(UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        // return the ip number as the value of the method
+        return getIP;
+
+    }
+    
 }
